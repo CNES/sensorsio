84986a30805367f70abbbe689f2a07a722b4c074
Julien Michel <julien.michel@orfeo-toolbox.org>
2021-06-23 15:30:55 +0000

    ENH: Add functions to read angles masks and values

--- src/sensorsio/sentinel2.py
+++ src/sensorsio/sentinel2.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 # Copyright: (c) 2021 CESBIO / Centre National d'Etudes Spatiales
 
+from memory_profiler import profile
+
 import warnings
 from enum import Enum
+from collections import namedtuple
 import dateutil
 from typing import List, Tuple, Union
 import glob
@@ -16,7 +19,8 @@ import xml.etree.ElementTree as ET
 import rasterio as rio
 from shapely import geometry
 from sensorsio import utils
-
+from scipy import ndimage
+from sklearn.linear_model import LinearRegression
 
 warnings.filterwarnings("ignore", category=RuntimeWarning, module='geopandas')
 
@@ -105,6 +109,10 @@ class Sentinel2:
         # Get crs
             self.crs = ds.crs
 
+        # Init angles
+        self.sun_angles = None
+        self.incidence_angles = None
+            
         # Parse xml file if requested
         if parse_xml:
             self.parse_xml()
@@ -129,7 +137,33 @@ class Sentinel2:
                 self.orbit = int(orbit_node.text)
                 self.relative_orbit_number = self.compute_relative_orbit_number(self.orbit)
 
-                 
+            # Internal parsing function for angular grids
+            def parse_angular_grid_node(node):
+                    values=[]
+                    for c in node.find('Values_List'):
+                        values.append(np.array([float(t) for t in c.text.split()]))
+                        values_array = np.stack(values)
+                    return values_array
+
+            # Parse sun angles
+            Angles = namedtuple('Angles', 'zenith azimuth')
+
+            self.sun_angles = Angles(parse_angular_grid_node(root.find('.//Angles_Grids_List/Sun_Angles_Grids/Zenith')),
+                                     parse_angular_grid_node(root.find('.//Angles_Grids_List/Sun_Angles_Grids/Azimuth')))
+
+            # Parse incidence angles
+            self.incidence_angles = {}
+            for b in root.find('.//Angles_Grids_List/Viewing_Incidence_Angles_Grids_List'):
+                band_key = self.Band(b.attrib['band_id'])
+                band_dict = {}
+                for d in b.findall('Viewing_Incidence_Angles_Grids'):
+                    det_key=self.Detector(int(d.attrib['detector_id']))
+                    zen = parse_angular_grid_node(d.find('Zenith'))
+                    az = parse_angular_grid_node(d.find('Azimuth'))
+                    band_dict[det_key]=Angles(zen, az)
+                self.incidence_angles[band_key]=band_dict
+                                                                    
+            
     def compute_relative_orbit_number(self, orbit):
         """
         Compute relative orbit number from absolute orbit and sensor id
@@ -222,10 +256,25 @@ class Sentinel2:
     class BandType(Enum):
         FRE = 'FRE'
         SRE = 'SRE'
-
+        
     # Aliases for band type
     FRE = BandType.FRE
     SRE = BandType.SRE
+
+    # Detectors
+    class Detector(Enum):
+        D01 = 1
+        D02 = 2
+        D03 = 3
+        D04 = 4
+        D05 = 5
+        D06 = 6
+        D07 = 7
+        D08 = 8
+        D09 = 9
+        D10 = 10
+        D11 = 11
+        D12 = 12
     
     # MTF
     MTF = {
@@ -262,7 +311,7 @@ class Sentinel2:
         half_kernel_width: int = None):
         """
         Generate PSF kernels from list of bands
-
+    
         :param bands: A list of Sentinel2 Band Enum to generate PSF kernel for
         :param resolution: Resolution at which to sample the kernel
         :param half_kernel_width: The half size of the kernel
@@ -276,6 +325,26 @@ class Sentinel2:
                                                     Sentinel2.MTF[b],
                                                     half_kernel_width)) for b in bands])
 
+
+    def build_detectors_masks_path(self):
+        """
+        Return a dictionnary of path to detector masks at both R1 and R2 resolution
+        """
+         # Sorted ensure masks come in the same order in both lists
+        r1_masks = sorted(glob.glob(f"{self.product_dir}/MASKS/*DTF_R1-D*.tif"))
+        r2_masks = sorted(glob.glob(f"{self.product_dir}/MASKS/*DTF_R2-D*.tif"))
+
+        # Named tuple to store output
+        DetectorMasks = namedtuple('DetectorMasks', 'r1 r2')
+
+        output = {}
+
+        # Collect all pairs of detectors
+        for (r1, r2) in zip(r1_masks, r2_masks):
+            detector_idx = self.Detector(int(r1[-6:-4]))
+            output[detector_idx] = DetectorMasks(r1,r2)
+
+        return output
         
     def build_xml_path(self) -> str:
         """
@@ -484,3 +553,171 @@ class Sentinel2:
                                   'type' : band_type.value,
                                   'crs' : crs})
         return xarr
+
+    #@profile
+    def upsample_angular_grid(self, grid:np.ndarray, res:Res=Res.R1, order:int=1) -> np.ndarray:
+        """
+        upsample given angular grid at target resolution
+        """
+        if res == self.Res.R1:
+            target_resolution=10.
+        else:
+            target_resolution=20.
+        
+        # Angular grids have a resolution of 5 km
+        scale_factor=5000/target_resolution
+
+        input_grid = np.array(grid)
+                
+        zoomed_grid = ndimage.zoom(input_grid, scale_factor, prefilter=False, order=1, mode='nearest', grid_mode=True)
+        nb_pixels = int(10980*10/target_resolution)
+        # We assume that angular center of first pixel correspond to center of first 10m pixel
+        offset = int(scale_factor/2.)
+        return zoomed_grid[offset:nb_pixels+offset, offset:nb_pixels+offset]
+
+    #@profile
+    def extrapolate_grid(self,grid):
+        """
+        """
+        out_grid = np.copy(grid)
+        x, y = np.indices(grid.shape)
+        xvalid = x[~np.isnan(grid)]
+        xvalid = x[~np.isnan(grid)]
+        reg = LinearRegression().fit(np.stack((x[~np.isnan(grid)], y[~np.isnan(grid)]), axis=1),grid[~np.isnan(grid)])
+        grid_filled =  reg.predict(np.stack((x.ravel(), y.ravel()), axis=1)).reshape(grid.shape)
+        out_grid[np.isnan(grid)]=grid_filled[np.isnan(grid)]
+
+        return out_grid
+    
+    #@profile
+    def upsample_by_viewing_directions(self, zenith, azimuth, res:Res=Res.R1, order:int=1, extrapolate=False):
+        """
+
+        """
+        # Copy input grids
+        in_zenith=zenith
+        in_azimuth=azimuth
+        
+        # Extrapolate nans if needed
+        if extrapolate:
+            in_zenith=self.extrapolate_grid(zenith)
+            in_azimuth=self.extrapolate_grid(azimuth)
+            
+        # Use cartesian coordiantes to interpolate azimuth
+        dx = np.tan(np.deg2rad(in_zenith))* np.sin(np.deg2rad(in_azimuth))
+        dy = np.tan(np.deg2rad(in_zenith))* np.cos(np.deg2rad(in_azimuth))
+
+        zoomed_dx = self.upsample_angular_grid(dx,res=res, order=order)
+        zoomed_dy = self.upsample_angular_grid(dy,res=res, order=order)
+
+        # General case
+        zoomed_azimuth = np.arctan(zoomed_dx/zoomed_dy)
+        zoomed_azimuth[zoomed_dy<0] += np.pi
+        zoomed_azimuth[zoomed_azimuth<0] += 2*np.pi
+        zoomed_zenith = np.arctan(zoomed_dy/np.cos(zoomed_azimuth))
+
+        # dy == 0 but not dx
+        mask =np.logical_and(zoomed_dy==0, zoomed_dx!=0)
+        zoomed_azimuth[mask]=np.pi/(2*np.sign(zoomed_dx[mask]))
+        zoomed_azimuth[np.logical_and(mask,zoomed_azimuth<0)]+=2*np.pi
+        zoomed_zenith[mask]=np.arctan(np.abs(zoomed_dx[mask]))
+
+        # dy == 0 and dxy == 0
+        mask = np.logical_and(zoomed_dy==0, zoomed_dx==0)
+        zoomed_azimuth[mask]=0.
+        zoomed_zenith[mask]=0.
+
+        # Final conversion to degrees
+        zoomed_zenith=np.rad2deg(zoomed_zenith)
+        zoomed_azimuth=np.rad2deg(zoomed_azimuth)
+        
+        return zoomed_zenith, zoomed_azimuth
+
+    #@profile
+    def read_solar_angles_as_numpy(self, res:Res = Res.R1, interpolation_order:int=1):
+        """
+        Return zenith and azimuth solar angle as a tuple fo 2 numpy arrays at requested resolution
+        """
+        # Ensure that xml is parsed
+        if self.sun_angles is None:
+            self.parse_xml()
+
+        # Call up-sampling routine
+        return self.upsample_by_viewing_directions(self.sun_angles.zenith, self.sun_angles.azimuth, res, order=interpolation_order)
+
+    #@profile
+    def read_incidence_angles_as_numpy(self, band:Band = Band.B2, res:Res = Res.R1, interpolation_order:int=1):
+        """
+        
+        """
+        # Ensure that xml is parsed
+        if self.incidence_angles is None:
+            self.parse_xml()
+
+        band_angles = self.incidence_angles[band]
+
+        # Get path to detector masks
+        detector_masks = self.build_detectors_masks_path()
+
+        # Derive output shape
+        out_shape = (10980,10980)
+
+        if res != self.Res.R1:
+            out_shape=(5490,5490)
+
+        # Init outputs
+        odd_zenith_angles = np.full(out_shape,np.nan)
+        odd_azimuth_angles = np.full(out_shape,np.nan)
+        even_zenith_angles = np.full(out_shape,np.nan)
+        even_azimuth_angles = np.full(out_shape,np.nan)
+
+        # Loop on all detectors
+        for det,angles in band_angles.items():
+
+            if det in detector_masks:
+                # Get path to detector mask
+                current_detector_mask_path = detector_masks[det].r1
+                if res != self.Res.R1:
+                    current_detector_mask_path = detector_masks[det].r2
+
+                # Read the mask
+                with rio.open(current_detector_mask_path) as ds:
+                    current_detector_mask = ds.read(1)
+
+                zoomed_zenith, zoomed_azimuth = self.upsample_by_viewing_directions(angles.zenith, angles.azimuth, res=res, order=interpolation_order, extrapolate=True)
+
+                # # Derive cartesian coordinates for azimyth
+                # cartesian = angles.zenith * np.cos(angles.azimuth*np.pi/180.)
+
+                # # Zoom zenith
+                # zoomed_zenith = self.upsample_angular_grid(angles.zenith,res=res, order=interpolation_order, extrapolate=True)
+                
+                # # Zoom cartesian
+                # zoomed_cartesian = self.upsample_angular_grid(cartesian,res=res, order=interpolation_order, extrapolate=True)
+                
+                # # Get back to azimuth
+                # zoomed_azimuth = (180./np.pi)*np.arccos(zoomed_cartesian/zoomed_zenith)
+
+                # Apply masking
+                zoomed_zenith[current_detector_mask==0]=np.nan
+                zoomed_azimuth[current_detector_mask==0]=np.nan
+
+                # Build validy mask
+                valid_zenith_mask = np.logical_not(np.isnan(zoomed_zenith))
+                valid_azimuth_mask = np.logical_not(np.isnan(zoomed_azimuth))
+                                               
+                # Sort out detectors
+                if det.value%2==1:
+                    odd_zenith_angles[valid_zenith_mask] = zoomed_zenith[valid_zenith_mask]
+                    odd_azimuth_angles[valid_azimuth_mask] = zoomed_azimuth[valid_azimuth_mask]
+                else:
+                    even_zenith_angles[valid_zenith_mask] = zoomed_zenith[valid_zenith_mask]
+                    even_azimuth_angles[valid_azimuth_mask] = zoomed_azimuth[valid_azimuth_mask]
+
+                # Clear
+                del zoomed_zenith
+                del zoomed_azimuth
+                del valid_zenith_mask
+                del valid_azimuth_mask
+                
+        return even_zenith_angles, odd_zenith_angles, even_azimuth_angles, odd_azimuth_angles
