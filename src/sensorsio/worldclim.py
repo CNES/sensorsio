a638afaab29f74fb39231609c5f60e78c0fa2e79
Jordi Inglada <jordi.inglada@cesbio.eu>
2022-05-24 14:46:03 +0000

    Read only selected vars

--- src/sensorsio/worldclim.py
+++ src/sensorsio/worldclim.py
@@ -3,7 +3,8 @@
 # Copyright: (c) 2022 CESBIO / Centre National d'Etudes Spatiales
 """ Modeling and access tools for WorldClim 2.0 data """
 
-from typing import List, Tuple
+from enum import Enum
+from typing import List, Optional, Tuple, Union
 
 import numpy as np
 import rasterio as rio
@@ -11,6 +12,61 @@ from rasterio.coords import BoundingBox
 from rasterio.warp import reproject
 
 from .utils import compute_latlon_bbox_from_region
+
+
+class WorldClimQuantity(Enum):
+    PREC = "prec"
+    SRAD = "srad"
+    TAVG = "tavg"
+    TMAX = "tmax"
+    TMIN = "tmin"
+    VAPR = "vapr"
+    WIND = "wind"
+
+
+WorldClimQuantityAll = [wcq for wcq in WorldClimQuantity]
+
+
+class WorldClimBio(Enum):
+    BIO01 = "01"  # Annual Mean Temperature
+    BIO02 = "02"  # Mean Diurnal Range (Mean of monthly (max temp - min temp))
+    BIO03 = "03"  # Isothermality (BIO2/BIO7) (* 100)
+    BIO04 = "04"  # Temperature Seasonality (standard deviation *100)
+    BIO05 = "05"  # Max Temperature of Warmest Month
+    BIO06 = "06"  # Min Temperature of Coldest Month
+    BIO07 = "07"  # Temperature Annual Range (BIO05-BIO06)
+    BIO08 = "08"  # Mean Temperature of Wettest Quarter
+    BIO09 = "09"  # Mean Temperature of Driest Quarter
+    BIO10 = "10"  # Mean Temperature of Warmest Quarter
+    BIO11 = "11"  # Mean Temperature of Coldest Quarter
+    BIO12 = "12"  # Annual Precipitation
+    BIO13 = "13"  # Precipitation of Wettest Month
+    BIO14 = "14"  # Precipitation of Driest Month
+    BIO15 = "15"  # Precipitation Seasonality (Coefficient of Variation)
+    BIO16 = "16"  # Precipitation of Wettest Quarter
+    BIO17 = "17"  # Precipitation of Driest Quarter
+    BIO18 = "18"  # Precipitation of Warmest Quarter
+    BIO19 = "19"  # Precipitation of Coldest Quarter
+
+
+WorldClimBioAll = [wcb for wcb in WorldClimBio]
+
+
+class WorldClimVar:
+    """ WorldClim variable (either climatic quantity or bio)"""
+
+    def __init__(self,
+                 var: Union[WorldClimQuantity, WorldClimBio],
+                 month: Optional[int] = None):
+        self.value = var.value
+        if (month is None) and isinstance(var, WorldClimBio):
+            self.typ = 'bio'
+        elif (1 <= month <= 12) and isinstance(var, WorldClimQuantity):
+            self.typ = 'clim'
+            self.month = month
+        else:
+            raise ValueError("Quantity needs month. Bio does not use month. "
+                             "Received {var.value} {month}")
 
 
 class WorldClimData:
@@ -29,22 +85,12 @@ class WorldClimData:
         self.resolution = 30 / 60 / 60  # convert to degrees
 
         months = range(1, 13)
-        self.climvars = [
-            "prec",
-            "tavg",
-            "tmin",
-            "wind",
-            "srad",
-            "tmax",
-            "vapr",
-        ]
         self.climfiles = [
-            f"{wcdir}/{wcprefix}_{self.wcres}_{cv}_{m:02}.tif" for m in months
-            for cv in self.climvars
+            self.get_file_path(WorldClimVar(cv, m)) for m in months
+            for cv in WorldClimQuantityAll
         ]
-        biovars = range(1, 20)
         self.biofiles = [
-            f"{wcdir}/{wcprefix}_bio_{self.wcres}_{b:02}.tif" for b in biovars
+            self.get_file_path(WorldClimVar(b)) for b in WorldClimBioAll
         ]
 
         with rio.open(self.climfiles[0]) as ds:
@@ -68,11 +114,24 @@ class WorldClimData:
 
         return image
 
-    def get_wc_for_bbox(self, bbox) -> np.array:
+    def get_file_path(self, var: WorldClimVar):
+        """ Return the file path for a variable"""
+        if var.typ == 'bio':
+            fname = f"bio_{self.wcres}_{var.value}"
+        else:
+            fname = f"{self.wcres}_{var.value}_{var.month:02}"
+        return f"{self.wcdir}/{self.wcprefix}_{fname}.tif"
+
+    def get_wc_for_bbox(self,
+                        bbox,
+                        vars: Optional[List[WorldClimVar]] = None) -> np.array:
         "Get a stack with all the WC vars croped to contain the bbox"
+        if vars is None:
+            files = self.climfiles + self.biofiles
+        else:
+            files = [self.get_file_path(v) for v in vars]
         wcvars: List[np.array] = [
-            self.crop_to_bbox(v, bbox)[0, :, :]
-            for v in self.climfiles + self.biofiles
+            self.crop_to_bbox(wc_file, bbox)[0, :, :] for wc_file in files
         ]
         transform = rio.Affine(self.transform.a, self.transform.b, bbox.left,
                                self.transform.d, self.transform.e, bbox.top)
