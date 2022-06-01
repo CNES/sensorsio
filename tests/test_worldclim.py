30c791a9bef74851b02e47386e3d0995f4d14d01
Jordi Inglada <jordi.inglada@cesbio.eu>
2022-06-01 07:24:35 +0000

    Parameterize read_as_numpy test

--- tests/test_worldclim.py
+++ tests/test_worldclim.py
@@ -80,35 +80,41 @@ def test_get_wc_for_bbox(vars):
 
 
 @pytest.mark.parametrize(
-    "vars",
+    "vars, suffix",
     [
-        None,
-        [WorldClimVar(WorldClimQuantity.PREC, m) for m in range(1, 6)],
-        [WorldClimVar(wcb) for wcb in WorldClimBio],
+        (None, "all"),
+        ([WorldClimVar(WorldClimQuantity.PREC, m)
+          for m in range(1, 6)], "prec"),
+        ([WorldClimVar(wcb) for wcb in WorldClimBio], "bio"),
     ],
 )
-def test_wc_read_as_numpy(vars):
+def test_wc_read_as_numpy(vars, suffix):
     TILE = "35NKA"
     crs = mgrs.get_crs_mgrs_tile(TILE)
     resolution = 200.0
     bbox = mgrs.get_bbox_mgrs_tile(TILE, latlon=False)
     wcd = WorldClimData()
-    (dst_wc, xcoords, ycoords, crs, dst_wc_transform) = wcd.read_as_numpy(
-        vars=vars, crs=crs, resolution=resolution, bounds=bbox
-    )
+    (dst_wc, xcoords, ycoords, crs,
+     dst_wc_transform) = wcd.read_as_numpy(vars=vars,
+                                           crs=crs,
+                                           resolution=resolution,
+                                           bounds=bbox)
+    expected_bands = len(wcd.climfiles +
+                         wcd.biofiles) if vars is None else len(vars)
+    assert dst_wc.shape[0] == expected_bands
     # Write just 3 channels for simplicity
     dst_wc = dst_wc[:3, :, :]
     with rio.open(
-        "/work/scratch/ingladaj/MMDC/wc_test.tif",
-        "w",
-        driver="GTiff",
-        height=dst_wc.shape[1],
-        width=dst_wc.shape[2],
-        count=dst_wc.shape[0],
-        nodata=-32768.0,
-        dtype=dst_wc.dtype,
-        compress="lzw",
-        crs=crs,
-        transform=dst_wc_transform,
+            f"/work/scratch/{os.environ['USER']}/wc_test_{suffix}.tif",
+            "w",
+            driver="GTiff",
+            height=dst_wc.shape[1],
+            width=dst_wc.shape[2],
+            count=dst_wc.shape[0],
+            nodata=-32768.0,
+            dtype=dst_wc.dtype,
+            compress="lzw",
+            crs=crs,
+            transform=dst_wc_transform,
     ) as ds:
         ds.write(dst_wc)
