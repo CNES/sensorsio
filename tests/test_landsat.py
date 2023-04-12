ed410356e5b040a59e9b561329154fa16ac9fc89
Julien Michel <julien.michel@cesbio.cnes.fr>
2023-04-12 17:09:24 +0200

    TEST: Add tests for Landsat driver (WIP)

---
+++ tests/test_landsat.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2023 CESBIO / Centre National d'Etudes Spatiales
+"""
+This module contains tests for the Landsat-8 driver
+"""
+import datetime
+import os
+from dataclasses import dataclass, field
+from typing import List, Optional, Tuple, Union
+
+import numpy as np
+import pytest
+import rasterio as rio
+from sensorsio import landsat
+
+
+def get_landsat_c2L2_folder() -> str:
+    """
+    Retrieve SRTM folder from env var
+    """
+    return os.path.join(os.environ['SENSORSIO_TEST_DATA_PATH'], 'landsat8', 'c2l2',
+                        'LC08_L2SP_118051_20200206_20200823_02_T2')
+
+
+@pytest.mark.requires_test_data
+def test_venus_instantiate_c2l2():
+    """
+    Test sentinel2 class instantiation 
+    """
+    ls8 = landsat.Landsat(get_landsat_c2L2_folder())
+
+    assert ls8.product_dir == get_landsat_c2L2_folder()
+    assert ls8.product_name == 'LC08_L2SP_118051_20200206_20200823_02_T2'
+    assert ls8.date == datetime.datetime(2020, 2, 6)
+    assert ls8.year == 2020
+    assert ls8.day_of_year == 37
+    assert ls8.crs == 'epsg:32650'
+    assert ls8.bounds == rio.coords.BoundingBox(left=437085.0,
+                                                bottom=1322085.0,
+                                                right=664215.0,
+                                                top=1554315.0)
+
+
+@dataclass(frozen=True)
+class ReadAsNumpyParams:
+    """
+    Class to store read_as_numpy parameters
+    """
+    bands: List[landsat.Landsat.Band] = field(
+        default_factory=lambda: landsat.Landsat.GROUP_SR + landsat.Landsat.GROUP_ST)
+    masks: List[landsat.Landsat.Mask] = field(default_factory=lambda: landsat.Landsat.ALL_MASKS)
+    crs: Optional[str] = None
+    resolution: float = 10
+    region: Union[Tuple[int, int, int, int], rio.coords.BoundingBox] = None
+    no_data_value: float = np.nan
+    bounds: rio.coords.BoundingBox = None
+    algorithm: rio.enums.Resampling = rio.enums.Resampling.cubic
+    dtype: np.dtype = np.dtype('float32')
+
+    def expected_shape(self) -> Tuple[int, int]:
+        """
+        return expected shape
+        """
+        if self.region is not None:
+            if isinstance(self.region, rio.coords.BoundingBox):
+                return (int((self.region[3] - self.region[1]) / self.resolution),
+                        int((self.region[2] - self.region[0]) / self.resolution))
+            return (int((self.region[3] - self.region[1])), int((self.region[2] - self.region[0])))
+        if self.bounds is not None:
+            return (int((self.bounds[3] - self.bounds[1]) / self.resolution),
+                    int((self.bounds[2] - self.bounds[0]) / self.resolution))
+
+        return (int(10980 * 10 / self.resolution), int(10980 * 10 / self.resolution))
+
+
+@pytest.mark.requires_test_data
+@pytest.mark.parametrize(
+    "parameters",
+    [
+        # Use region to restrict source reading with bounding box
+        ReadAsNumpyParams(region=rio.coords.BoundingBox(
+            left=354650., bottom=4828620., right=355650., top=4829620.)),
+        # Use region to restrict source read with pixel coords
+        ReadAsNumpyParams(region=[0, 0, 100, 200]),
+        # Use bounds to set output region
+        ReadAsNumpyParams(bounds=rio.coords.BoundingBox(
+            left=354650., bottom=4828620., right=355650., top=4829620.)),
+        # Set a different target crs
+        ReadAsNumpyParams(bounds=rio.coords.BoundingBox(
+            left=554540.0, bottom=6279120.0, right=555540.0, top=6280120.0),
+                          crs='EPSG:2154')
+    ])
+def test_read_as_numpy_xarray(parameters: ReadAsNumpyParams):
+    """
+    Test the read_as_numpy method
+    """
+    ls8_dataset = landsat.Landsat(get_landsat_c2L2_folder())
+
+    # Read as numpy part
+    bands_arr, mask_arr, xcoords, ycoords, crs = ls8_dataset.read_as_numpy(**parameters.__dict__)
+
+    assert bands_arr.shape == (len(parameters.bands), *parameters.expected_shape())
+    assert mask_arr.shape == (len(parameters.masks), *parameters.expected_shape())
+    assert (~np.isnan(bands_arr)).sum() > 0
+
+    assert ycoords.shape == (parameters.expected_shape()[0], )
+    assert xcoords.shape == (parameters.expected_shape()[1], )
+
+    if parameters.crs is not None:
+        assert crs == parameters.crs
+    else:
+        assert crs == 'epsg:32631'
+
+    # Test read as xarray part
+    ls8_xr = ls8_dataset.read_as_xarray(**parameters.__dict__)
+
+    for c in ['t', 'x', 'y']:
+        assert c in ls8_xr.coords
+
+    assert ls8_xr['t'].shape == (1, )
+    assert ls8_xr['x'].shape == (parameters.expected_shape()[1], )
+    assert ls8_xr['y'].shape == (parameters.expected_shape()[0], )
+
+    for band in parameters.bands:
+        assert band.value in ls8_xr.variables
+        assert ls8_xr[band.value].shape == (1, *parameters.expected_shape())
+
+    if parameters.crs is not None:
+        assert ls8_xr.attrs['crs'] == parameters.crs
+    else:
+        assert ls8_xr.attrs['crs'] == ls8_dataset.crs
