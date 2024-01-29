aca7c1617b7b979ed01673bc0cdbe869a10423e6
Emmanuelle Sarrazin <emmanuelle.sarrazin@cnes.fr>
2024-01-29 15:52:47 +0100

    BUG: add data directory with package installation

--- setup.py
+++ setup.py
@@ -6,11 +6,22 @@
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
+
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setup(
+			use_scm_version={"version_scheme": "no-guess-dev"},
+            install_requires=['setuptools > 69.0.0'],
+            packages=find_namespace_packages(where="src"),
+            package_dir={"": "src"},
+            include_package_data=True,
+            package_data={
+                "sensorsio.data.sentinel2": ["*.gpkg","*.csv","*.zip"],
+                "sensorsio.data.venus": ["*.gpkg","*.csv","*.txt"],
+            }
+        )
     except:  # noqa
         print("\n\nAn error occurred while building the project, "
               "please ensure you have the most updated version of setuptools, "
