42b7d80efa6cb20a2c3a3490df88d02436f76160
Eve <epoitevin@groupcls.com>
2025-02-28 10:04:46 +0100

    add support for images starting with 'sentinel2c'

--- src/sensorsio/sentinel2_l3a.py
+++ src/sensorsio/sentinel2_l3a.py
@@ -111,11 +111,13 @@ class Sentinel2L3A:
         S2X = "SENTINEL2X"
         S2A = "SENTINEL2A"
         S2B = "SENTINEL2B"
+        S2C = "SENTINEL2C"
 
     # Aliases
     S2X = Satellite.S2X
     S2A = Satellite.S2A
     S2B = Satellite.S2B
+    S2C = Satellite.S2C
 
     class Band(Enum):
         """
